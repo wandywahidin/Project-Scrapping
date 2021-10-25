bd8a5d82857d5bb663065f3b65b7f6f478d76f5a
wandy <wandywahidin53@gmail.com>
2021-10-26 04:28:33 +0700

    setup request and beautifulsoup4

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Aplikasi deteksi gempa terkini
+"""
+
+
+def ekstraksi_data():
+    pass
+
+
+def tampilkan_data(result):
+    pass
+
+
+if __name__ == '__main__':
+    print('Aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
